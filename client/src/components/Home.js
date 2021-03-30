a2a2b33e509a8411f1f7592a9899039417f25109
Pragya <pragyamaheshwari2305@gmail.com>
2021-03-30 13:04:34 +0530

    added navbar

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+const Home=()=> {
+    return (
+        <div className='intro-main' id ='home'>
+            <div className='container'>
+                <h1>Welcome To WEBSITE Name</h1>
+                <br />
+                (or anything catchy)
+            </div>
+        </div>
+    );
+}
+
+export default Home
