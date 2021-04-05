41e2e5ed828ad327cf9f547dea4f5b852d431a9a
saumya-aghera <78028920+saumya-aghera@users.noreply.github.com>
2021-04-05 16:41:51 +0530

    Add files via upload

---
+++ client/src/components/Modules/Module.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Module.css'
+function Module() {
+    return (
+        <>
+            <div className="modules">
+                <h2 className="mod-h2">Modules</h2>
+                <div id="outer">
+  <div className="inner"><form action="/Hope" target="_blank"><button type="submit" className=" bt msgBtn1" ><span>Module 1: <br /> Hope</span></button></form></div>
+  <div className="inner"><form action="/Hope" target="_blank"><button type="submit" className="bt msgBtn4"><span>Module 2: <br /> Hope</span></button></form></div>
+  <div className="inner"><form action="/Hope" target="_blank"><button className="bt msgBtn3"><span>Module 3: <br /> Hope</span></button></form></div>
+  <div className="inner"><form action="/Hope" target="_blank"><button type="submit" className="bt msgBtn2"><span>Module 4: <br /> Hope</span></button></form></div>
+  <div className="inner"><form action="/Hope" target="_blank"><button className="bt msgBtn5"><span>Module 5: <br /> Hope</span></button></form></div>
+  <div className="inner"><form action="/Hope" target="_blank"><button type="submit" className="bt msgBtn6"><span>Module 6: <br /> Hope</span></button></form></div>
+  {/* <div className="inner"><button className="bt msgBtn7">Module 7: <br /> xyz</button></div> */}
+</div>
+            </div>
+            
+        </>
+    )
+}
+
+export default Module
