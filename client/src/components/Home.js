9eac5e4dc8b4ba9c4a66f39037c067c3089a2447
saumya-aghera <78028920+saumya-aghera@users.noreply.github.com>
2021-04-06 17:22:00 +0530

    Add files via upload

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,21 +2,24 @@ import React from 'react'
 import './Home.css'
 import Intro from '././Intro'
 import Module from '././Modules/Module'
-
+import vid from '../images/vid.mp4'
 
 const Home=()=> {
     return (
         
         <div className='home-main' id ='home'>
             {/* <img src="../images/pinkmh.png" style={{width:'100%', height:'1000px'}}/> */}
-            <div className='container-home'>
+            {/* <div className='container-home'> */}
+                <video autoPlay loop muted id='video'>
+                    <source src={vid} type='video/mp4'/>
+                </video>
                 <div className="home-text">
-                <h1>Welcome To WEBSITE Name</h1>
+                <h1 className="home-h1">HAPPETITE</h1>
                 <br />
-                (or anything catchy)
+                <h4>Feed Your Happiness</h4>
                 </div>
                 
-            </div>
+            {/* </div> */}
             <Intro />
             <Module />
            
