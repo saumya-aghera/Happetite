fd6871f5dd1889fcddd718135e0d9f1ddf7da3ce
saumya-aghera <78028920+saumya-aghera@users.noreply.github.com>
2021-04-07 12:30:18 +0530

    Add files via upload

--- client/src/components/Hope/Hope.js
+++ client/src/components/Hope/Hope.js
@@ -3,12 +3,20 @@ import Video from './Video';
 import HopeBox from './HopeBox';
 import Quiz from './Quiz';
 import HomeAssign from './HomeAssign';
+import Worksheet from './Worksheet';
 
 const Hope = ({ loggedIn,onLogin,user,setUser }) => {
     
     return (
         <div>
             <Video />
+            <Worksheet
+                loggedIn={loggedIn}
+                onLogin={onLogin}
+                user={user}
+                setUser={setUser}
+                
+            />
             <HopeBox
                 loggedIn={loggedIn}
                 onLogin={onLogin}
