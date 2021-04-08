1dd2e95760d05c31ee40f0e7da368b59673768aa
saumya-aghera <78028920+saumya-aghera@users.noreply.github.com>
2021-04-08 22:52:48 +0530

    Add files via upload

--- client/src/components/Hope/Video.js
+++ client/src/components/Hope/Video.js
@@ -6,6 +6,8 @@ function Video(){
     return (
         <div className="vid-main" id="hopeVideo">
             <div className='outer-box1'>
+                <h2>What is Hope?</h2>
+                <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSnHPMR2Cv7Fd7fVWA4cHA_sYLNYZBxAd167NHGeLgbSIe7DjgX6YynT-nwqS7DVTd41SuXMSMHQWnn/embed?start=false&loop=false&delayms=5000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                 <h2>Hope Theory</h2>
                 <div className="video">
                     <ReactPlayer
