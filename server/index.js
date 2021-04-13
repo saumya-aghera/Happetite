9d8bb584077c6a137f32a729f3acdf3d37562324
saumya-aghera <78028920+saumya-aghera@users.noreply.github.com>
2021-04-13 13:04:42 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ import H3Routes from './routes/h3.js';
 import TryRoutes from './routes/try.js';
 import LetRoutes from './routes/letter.js';
 import ThankRoutes from './routes/thankful.js';
+import HW4_1 from './routes/hw4_1.js';
+import HW4_2 from './routes/hw4_2.js';
+import HW4_3 from './routes/hw4_3.js';
+import HW4_4 from './routes/hw4_4.js';
+import HW4_5 from './routes/hw4_5.js';
+import HW4_6 from './routes/hw4_6.js';
+import HW4_7 from './routes/hw4_7.js';
 
 
 const app = express();
@@ -24,6 +31,14 @@ app.use('/h3', H3Routes);
 app.use('/tryy', TryRoutes);
 app.use('/letter', LetRoutes);
 app.use('/thankful', ThankRoutes);
+app.use('/hw4_1', HW4_1);
+app.use('/hw4_2', HW4_2);
+app.use('/hw4_3', HW4_3);
+app.use('/hw4_4', HW4_4);
+app.use('/hw4_5', HW4_5);
+app.use('/hw4_6', HW4_6);
+app.use('/hw4_7', HW4_7);
+
 
 
 
