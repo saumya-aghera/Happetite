bdec8c9ca966f0ea7cb592ad749ceddf30bfdcd5
saumya-aghera <78028920+saumya-aghera@users.noreply.github.com>
2021-06-27 13:12:12 +0530

    Add files via upload

--- client/src/components/Module5/CharStrength.js
+++ client/src/components/Module5/CharStrength.js
@@ -10,11 +10,7 @@ function CharStrength() {
                 <div className='ch-cont'>
             <h2>Introduction to Character Strengths</h2>
             <div className="video">
-                    <ReactPlayer
-                        className="vid-container"
-                        url="https://www.youtube.com/watch?v=BdQRECe37K0"
-                    
-                    />
+            <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRKPRSwff86FGAj8LqAQRzn_eATRGGL7A5Q6GlHmL92bnbnsdZ70vpWtT7kpX8XAScYXT8KUcv4iB2L/embed?start=true&loop=true&delayms=15000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                     </div>
                     </div>
         </div>
