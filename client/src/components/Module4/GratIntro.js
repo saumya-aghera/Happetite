db6aa163e9792a297776e0e90b4ce5624555731b
saumya-aghera <78028920+saumya-aghera@users.noreply.github.com>
2021-06-29 15:20:53 +0530

    Add files via upload

--- client/src/components/Module4/GratIntro.js
+++ client/src/components/Module4/GratIntro.js
@@ -18,11 +18,8 @@ function GratIntro() {
                 }}>Gratitude as we know, is a feeling of being appreciative or thankful for someone or something. This skill helps us focus on the positive aspects of our life. Let’s get a better understanding of it by watching the video -
 </div>
                 <div className="video">
-                    <ReactPlayer
-                        className="vid-container"
-                        url="https://www.youtube.com/watch?v=JMd1CcGZYwU"
-                    
-                    />
+                <iframe src="https://drive.google.com/file/d/1biv9ndt2L7ZQ7zf9pCI3nQusBbTejzAS/preview" width="700" height="480" allow="autoplay"></iframe>
+                  
                     </div>
         </div>
         </div>
